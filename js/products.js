649cf54f28345a05d985547a9b8587ba6c4b1e62
Danny0885 <danny0885@users.noreply.github.com>
2026-05-10 19:14:28 +0300

    Update ATH-M50x Thomann URL to direct product page

--- js/products.js
+++ js/products.js
@@ -455,7 +455,7 @@ const products = [
     img: "https://thumbs.static-thomann.de/thumb/thumb600x600/pics/prod/331905.jpg",
     stores: {
       sweetwater: "https://www.sweetwater.com/store/detail/ATHM50x--audio-technica-ath-m50x-closed-back-studio-monitoring-headphones",
-      thomann: "https://www.thomann.co.uk/search.html?sw=audio-technica%20ath-m50x",
+      thomann: "https://www.thomann.co.uk/audio_technica_ath_m50_x.htm",
       gear4music: "https://www.gear4music.com/PA-DJ-and-Lighting/Audio-Technica-ATH-M50x-Headphones-Black/X9G"
     }
   },
