6eff0127a66d6193b2c14ace086dd5310919394d
topmusiciangear <topmusiciangear@users.noreply.github.com>
2026-05-13 17:59:39 +0300

    use official favicons and styled icons for new EU stores

--- js/products.js
+++ js/products.js
@@ -1028,7 +1028,7 @@ const storeIcons = {
   musikproduktiv: '<img src="img/musikproduktiv-icon.png" alt="Musik Produktiv" class="store-icon-img" style="width:28px">',
   amazon: '<i class="fa-brands fa-amazon" style="font-size:15px;"></i>',
   reverb: "<span style='font-weight:900;font-size:14px;line-height:1;display:inline-flex;align-items:center;justify-content:center;width:16px;height:16px;background:#d6562b;color:#fff;border-radius:2px;vertical-align:middle;'>R</span>",
-  andertons: '<i class="fa-solid fa-guitar" style="font-size:15px;"></i>',
-  baxmusic: '<i class="fa-solid fa-drum" style="font-size:15px;"></i>',
-  musicstore: '<i class="fa-solid fa-store" style="font-size:15px;"></i>'
+  andertons: "<span style='font-weight:900;font-size:14px;line-height:1;display:inline-flex;align-items:center;justify-content:center;width:16px;height:16px;background:#00a8b0;color:#fff;border-radius:2px;vertical-align:middle;'>A</span>",
+  baxmusic: '<img src="img/baxmusic-icon.png" alt="Bax Music" class="store-icon-img">',
+  musicstore: '<img src="img/musicstore-icon.png" alt="Music Store" class="store-icon-img">'
 };
