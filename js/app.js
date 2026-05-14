74cb1bdc619faa4d0aad2efc6ba06b2a6c4226fc
topmusiciangear <topmusiciangear@users.noreply.github.com>
2026-05-14 09:07:51 +0300

    use official Music Store logo, update domain to musicstore.com

--- js/app.js
+++ js/app.js
@@ -142,7 +142,7 @@ function getResolvedStores(product) {
   s.reverb = `https://reverb.com/marketplace?query=${encodeURIComponent(product.title)}`;
   if (!product.stores.andertons) s.andertons = `https://www.andertons.co.uk/search.php?search_query=${encodeURIComponent(product.title)}`;
   if (!product.stores.baxmusic) s.baxmusic = `https://www.bax-shop.co.uk/catalogsearch/result/?q=${encodeURIComponent(product.title)}`;
-  if (!product.stores.musicstore) s.musicstore = `https://www.musicstore.com/catalogsearch/result/?q=${encodeURIComponent(product.title)}`;
+  if (!product.stores.musicstore) s.musicstore = `https://www.musicstore.com/search?q=${encodeURIComponent(product.title)}`;
   return s;
 }
 
