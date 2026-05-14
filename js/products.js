e43cc47fff8f420561798e1fd665a1279b73844c
Danny0885 <danny0885@users.noreply.github.com>
2026-05-14 15:39:45 +0300

    update Fender product links to uk.fender.com search

--- js/products.js
+++ js/products.js
@@ -1015,7 +1015,7 @@ const products = [
       thomann: "https://www.thomann.co.uk/fender_player_stratocaster_mn_blk.htm",
       gear4music: "https://www.gear4music.com/Guitar-and-Bass/Fender-Player-Stratocaster-Olympic-White/27HN",
       musikproduktiv: "https://www.musik-produktiv.de/search",
-      fender: "https://www.fender.com/en-US/electric-guitars/stratocaster/player-stratocaster/0144502500.html"
+      fender: "https://uk.fender.com/search?q=Fender+Player+Stratocaster"
     }
   },
   {
@@ -1035,7 +1035,7 @@ const products = [
       thomann: "https://www.thomann.co.uk/fender_player_telecaster_mn_blk.htm",
       gear4music: "https://www.gear4music.com/Guitar-and-Bass/Fender-Player-Telecaster-Black/27HR",
       musikproduktiv: "https://www.musik-produktiv.de/search",
-      fender: "https://www.fender.com/en-US/electric-guitars/telecaster/player-telecaster/0145212500.html"
+      fender: "https://uk.fender.com/search?q=Fender+Player+Telecaster"
     }
   },
   {
@@ -1055,7 +1055,7 @@ const products = [
       thomann: "https://www.thomann.co.uk/fender_american_professional_ii_telecaster.htm",
       gear4music: "https://www.gear4music.com/Guitar-and-Bass/Fender-American-Professional-II-Telecaster-Butterscotch-Blonde/5M4A",
       musikproduktiv: "https://www.musik-produktiv.de/search",
-      fender: "https://www.fender.com/en-US/electric-guitars/telecaster/american-professional-ii-telecaster/0113942700.html"
+      fender: "https://uk.fender.com/search?q=Fender+American+Professional+II+Telecaster"
     }
   },
   {
@@ -1075,7 +1075,7 @@ const products = [
       thomann: "https://www.thomann.co.uk/fender_american_ultra_stratocaster_csb.htm",
       gear4music: "https://www.gear4music.com/Guitar-and-Bass/Fender-American-Ultra-Stratocaster-Cosmic-Sapphire-Rosewood/6XIU",
       musikproduktiv: "https://www.musik-produktiv.de/search",
-      fender: "https://www.fender.com/en-US/electric-guitars/stratocaster/american-ultra-stratocaster/0118010712.html"
+      fender: "https://uk.fender.com/search?q=Fender+American+Ultra+Stratocaster"
     }
   },
   {
@@ -1095,7 +1095,7 @@ const products = [
       thomann: "https://www.thomann.co.uk/fender_player_ii_jazzmaster_3cs.htm",
       gear4music: "https://www.gear4music.com/Guitar-and-Bass/Fender-Player-II-Jazzmaster-3-Color-Sunburst-Maple/65A3",
       musikproduktiv: "https://www.musik-produktiv.de/search",
-      fender: "https://www.fender.com/en-US/electric-guitars/jazzmaster/player-ii-jazzmaster/0140590558.html"
+      fender: "https://uk.fender.com/search?q=Fender+Player+II+Jazzmaster"
     }
   },
   {
@@ -1115,7 +1115,7 @@ const products = [
       thomann: "https://www.thomann.co.uk/fender_player_precision_bass_mn_blk.htm",
       gear4music: "https://www.gear4music.com/Guitar-and-Bass/Fender-Player-Precision-Bass-Black/27HL",
       musikproduktiv: "https://www.musik-produktiv.de/search",
-      fender: "https://www.fender.com/en-US/electric-basses/precision-bass/player-precision-bass/0149803515.html"
+      fender: "https://uk.fender.com/search?q=Fender+Player+Precision+Bass"
     }
   },
   {
@@ -1135,7 +1135,7 @@ const products = [
       thomann: "https://www.thomann.co.uk/fender_player_jazz_bass_mn_blk.htm",
       gear4music: "https://www.gear4music.com/Guitar-and-Bass/Fender-Player-Jazz-Bass-Black/27HQ",
       musikproduktiv: "https://www.musik-produktiv.de/search",
-      fender: "https://www.fender.com/en-US/electric-basses/jazz-bass/player-jazz-bass/0149903500.html"
+      fender: "https://uk.fender.com/search?q=Fender+Player+Jazz+Bass"
     }
   },
   {
@@ -1155,7 +1155,7 @@ const products = [
       thomann: "https://www.thomann.co.uk/fender_cd_60s_dreadnought_nat.htm",
       gear4music: "https://www.gear4music.com/Guitar-and-Bass/Fender-CD-60S-Dreadnought-Acoustic-Natural/17FJ",
       musikproduktiv: "https://www.musik-produktiv.de/search",
-      fender: "https://www.fender.com/en-US/acoustic-guitars/dreadnought/cd-60s-dreadnought/0970110006.html"
+      fender: "https://uk.fender.com/search?q=Fender+CD-60S+Acoustic"
     }
   },
   {
@@ -1175,7 +1175,7 @@ const products = [
       thomann: "https://www.thomann.co.uk/fender_paramount_om_78e.htm",
       gear4music: "https://www.gear4music.com/Guitar-and-Bass/Fender-Paramount-OM-78E-Acoustic-Electric-Guitar-Natural/4YN4",
       musikproduktiv: "https://www.musik-produktiv.de/search",
-      fender: "https://www.fender.com/en-US/acoustic-guitars/orchestra/po-220e-orchestra/0970350321.html"
+      fender: "https://uk.fender.com/search?q=Fender+Paramount+OM-78E+Acoustic-Electric"
     }
   }
 ];
