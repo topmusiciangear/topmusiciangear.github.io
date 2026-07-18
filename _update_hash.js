16b978fb7c6e3012c06b124ba6894505d66a8c7d
Danny0885 <danny0885@users.noreply.github.com>
2026-07-18 17:25:43 +0300

    Remove 'Vamos pa la Disco' sentence from both languages

--- _update_hash.js
+++ _update_hash.js
@@ -1,6 +1,6 @@
 var fs = require('fs');
-var hash = require('crypto').createHash('md5').update(fs.readFileSync('js/app.min.js')).digest('hex').substring(0,8);
+var hash = require('crypto').createHash('md5').update(fs.readFileSync('js/translations.v4.min.js')).digest('hex').substring(0,12);
 var c = fs.readFileSync('index.html', 'utf8');
-c = c.replace(/(app\.min\.js\?v=)[a-f0-9]+/g, '$1' + hash);
+c = c.replace(/(translations\.v4\.min\.js\?v=)[a-f0-9]+/g, '$1' + hash);
 fs.writeFileSync('index.html', c);
-console.log('app hash:', hash);
+console.log('v4 hash:', hash);
