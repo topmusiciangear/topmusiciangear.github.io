55252e3afbf158f7f31a3b9d9019f4a20569ffd6
Danny0885 <danny0885@users.noreply.github.com>
2026-08-25 20:12:22 +0300

    Remove temp check scripts

--- _check_table_order.js
+++
@@ -1,5 +0,0 @@
-const g = require('./data/guides.json');
-g.filter(x => x.productTable).forEach(hub => {
-  const rows = hub.productTable.rows.map(r => r.label || r.label_en || Object.values(r)[0]);
-  console.log(hub.id + ':', rows.join(' | '));
-});
