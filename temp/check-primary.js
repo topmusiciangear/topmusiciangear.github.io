1c1a6ed8d2af9fa2354e833585d059b90712f589
Danny0885 <danny0885@users.noreply.github.com>
2026-08-27 21:33:54 +0300

    fix: Add Amazon URLs to DAW controller products - primary button now shows Amazon
    - Added Amazon URLs for IDs 395-400 (FaderPort 8, X-Touch, UF1, S1, Platform M+, MCU Pro)
    - Primary blue button now shows Amazon for non-USA users
    - zzounds only appears via geo-swap for USA users
    - 288 pages rebuilt, shop-buttons.js synced

--- temp/check-primary.js
+++
@@ -1,3 +0,0 @@
-var h = require('fs').readFileSync('C:\\Users\\Daniel\\projects\\topmusiciangear\\guides\\best-microphone.html', 'utf8');
-var idx = h.indexOf('class="shop-btn-primary"');
-console.log(h.substring(idx, idx + 1000));
