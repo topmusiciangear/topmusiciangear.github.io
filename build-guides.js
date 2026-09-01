48e0621c61524e95ad7b6387159980fdeb7d23e0
Danny0885 <danny0885@users.noreply.github.com>
2026-09-01 12:47:42 +0300

    feat: add DI Box guide with 7 products (Radial J48, RNDI, SansAmp, ProD2, Canvas Mono, Whirlwind IMP 2, LR Baggs Para)

--- build-guides.js
+++ build-guides.js
@@ -733,7 +733,14 @@ const TEST_SHOP_BTN = {
   441: {prices:{amazon:"$849.99",zzounds:"$849.99",gear4music:"£749.00",musicstore:"€898.00",andertons:"£799.00"}},
   442: {prices:{amazon:"$849.99",zzounds:"$849.99",gear4music:"£779.00",musicstore:"€849.00",andertons:"£779.00"}},
   443: {prices:{amazon:"$83.50",zzounds:"$83.50",gear4music:"£67.90",musicstore:"€89.00",andertons:"£69.99"}},
-  444: {prices:{amazon:"$849.99",zzounds:"$849.99",gear4music:"£699.00",musicstore:"€799.00",andertons:"£699.00"}}
+  444: {prices:{amazon:"$849.99",zzounds:"$849.99",gear4music:"£699.00",musicstore:"€799.00",andertons:"£699.00"}},
+  445: {prices:{amazon:"$259.99",zzounds:"$259.99",gear4music:"£219.00",musicstore:"€249.00",andertons:"£229.00"}},
+  446: {prices:{amazon:"$399.00",zzounds:"$399.00",gear4music:"£349.00"}},
+  447: {prices:{amazon:"$269.95",gear4music:"£249.00",musicstore:"€269.00",andertons:"£259.00"}},
+  448: {prices:{amazon:"$209.99",zzounds:"$209.99",gear4music:"£189.00",musicstore:"€222.00",andertons:"£198.00"}},
+  449: {prices:{amazon:"$149.99",musicstore:"€149.00",andertons:"£139.00"}},
+  450: {prices:{amazon:"$79.00",zzounds:"$79.00"}},
+  451: {prices:{amazon:"$229.00",zzounds:"$229.00",gear4music:"£199.00",musicstore:"€229.00",andertons:"£199.00"}}
 };;;;
 
 function shopButtonsTest(p, lang) {
