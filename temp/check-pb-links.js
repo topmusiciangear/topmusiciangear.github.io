7371552efc1074517a32f6ebf99377d0f8427edb
Danny0885 <danny0885@users.noreply.github.com>
2026-09-02 10:34:18 +0300

    chore: cleanup temp scripts

--- temp/check-pb-links.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const g = JSON.parse(fs.readFileSync('./data/guides.json', 'utf8'));
-const p = g.filter(x => x.category === 'plugins');
-p.forEach(x => {
-  const m = x.conclusion_es.match(/href="([^"]+)"[^>]*>Más información/);
-  console.log(x.id, m ? m[1] : 'NO LINK');
-});
