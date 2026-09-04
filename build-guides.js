9b6fde7a35c4ba39df79eed2580c917b1f4b0a75
Danny0885 <danny0885@users.noreply.github.com>
2026-09-04 10:19:44 +0300

    Launchkey Mini 25 MK4: fix G4M 102.75->115 + MS 115.97->111.00

--- build-guides.js
+++ build-guides.js
@@ -661,7 +661,7 @@ const TEST_SHOP_BTN = {
   321: {prices:{gear4music:"£508.00",amazon:"$467.00",zzounds:"$549.00",andertons:"£505.00",musicstore:"€438.66"}},
   322: {prices:{gear4music:"£599.00",amazon:"$639.49",zzounds:"$749.00",andertons:"£598.00",musicstore:"€923.53"}},
   323: {prices:{amazon:"$99.00",zzounds:"$99.99",andertons:"£91.00",gear4music:"£91.30",musicstore:"€105.04"},urls:{zzounds:"https://www.zzounds.com/item--AKAMPKMINI3"}},
-  324: {prices:{amazon:"$129.99",zzounds:"$129.99",andertons:"£89.00",gear4music:"£102.75",musicstore:"€115.97"}},
+  324: {prices:{amazon:"$129.99",zzounds:"$129.99",andertons:"£89.00",gear4music:"£115.00",musicstore:"€111.00"}},
   325: {prices:{gear4music:"£380.00",amazon:"$549.00",zzounds:"$549.00",musicstore:"€503.36",andertons:"£379.00"}},
   326: {prices:{amazon:"$499.00",zzounds:"$499.00",andertons:"£399.00",gear4music:"$499.00"}},
   327: {prices:{amazon:"$109.97"},oos:["andertons","musicstore"]},
