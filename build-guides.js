9d13761a3964217e569b952b6e555721a09d2151
Danny0885 <danny0885@users.noreply.github.com>
2026-09-09 16:57:50 +0300

    THR30II musicstore price 499.00 EUR

--- build-guides.js
+++ build-guides.js
@@ -822,7 +822,7 @@ const TEST_SHOP_BTN = {
    499: {prices:{zzounds:"$267.00",gear4music:"£354.00",musicstore:"€389.00"}},
    500: {prices:{zzounds:"$466.95",gear4music:"£418.00",musicstore:"€469.00"}},
    501: {prices:{gear4music:"£379.00",andertons:"£379.00",musicstore:"€399.00"}},
-   503: {prices:{gear4music:"£439.00",amazon:"$599.99",zzounds:"$599.99",andertons:"£439.00",musicstore:"€492.00"}}
+   503: {prices:{gear4music:"£439.00",amazon:"$599.99",zzounds:"$599.99",andertons:"£439.00",musicstore:"€499.00"}}
   };function shopButtonsTest(p, lang) {
   const cfg = TEST_SHOP_BTN[p.id] || {};
   const prices = cfg.prices || {};
