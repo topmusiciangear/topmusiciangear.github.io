693c2eedddb1d45ff92dc6ad404dff2a4d3fe72d
Danny0885 <danny0885@users.noreply.github.com>
2026-09-09 13:38:26 +0300

    cleanup: remove temp SM7dB restore artifacts from repo

--- temp/restore-sm7db.js
+++
@@ -1,7 +0,0 @@
-var fs = require('fs');
-var data = JSON.parse(fs.readFileSync('data/products.json', 'utf8'));
-var entry = JSON.parse(fs.readFileSync('temp/_sm7db_entry.json', 'utf8'))[0];
-if (data.some(function (e) { return e && e.id === 321; })) throw new Error('id 321 already present');
-data.push(entry);
-fs.writeFileSync('data/products.json', JSON.stringify(data, null, 2) + '\n');
-console.log('products.json:', data.length, 'entries; id 321', entry.title, 're-added');
