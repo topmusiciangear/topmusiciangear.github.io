c9ed83908505862d2401ffce60e24b807fca9560
Danny0885 <danny0885@users.noreply.github.com>
2026-09-09 16:08:14 +0300

    THR10II musicstore 320 + g4m 299, MS row enabled

--- js/shop-buttons.js
+++ js/shop-buttons.js
@@ -321,7 +321,7 @@ const TEST_SHOP_BTN = {
   290: {prices:{gear4music:"£99.99",amazon:"$149.00",zzounds:"$109.00"},oos:["andertons"]},
   291: {prices:{gear4music:"£149.00",amazon:"$168.00",zzounds:"$169.00",andertons:"£149.00",musicstore:"€167.00"},urls:{gear4music:"https://www.gear4music.com/Recording-and-Computers/Audio-Technica-AT2020USBX-Cardioid-Condenser-Microphone/528M"},oos:["gear4music"]},
   292: {prices:{amazon:"$103.00",zzounds:"$105.00",gear4music:"£84.20",andertons:"£85.00",musicstore:"€95.00"}},
-  293: {prices:{gear4music:"£302.50",amazon:"$329.99",zzounds:"$369.99",andertons:"£299.00",musicstore:"€333.00"}},
+  293: {prices:{gear4music:"£299.00",amazon:"$329.99",zzounds:"$369.99",andertons:"£299.00",musicstore:"€320.00"}},
   294: {prices:{gear4music:"£279.00",amazon:"$279.00",zzounds:"$349.00",musicstore:"€377.31",andertons:"£269.00"}},
   295: {prices:{amazon:"$295.99"},oos:["andertons"]},
   296: {prices:{amazon:"$699.00",andertons:"£799.00"}},
