2574344bd1d60b31a17330dce06e5327f83a1a4d
Danny0885 <danny0885@users.noreply.github.com>
2026-09-09 15:54:45 +0300

    Fender Rumble 40 V3 G4M 319 + musicstore 299

--- js/shop-buttons.js
+++ js/shop-buttons.js
@@ -499,7 +499,7 @@ const TEST_SHOP_BTN = {
   487: {prices:{amazon:"$585.00"},oos:["zzounds"]},
   488: {prices:{zzounds:"$349.99",amazon:"$318.99",andertons:"£319.00",gear4music:"£285.50"}},
   489: {prices:{zzounds:"$199.00",amazon:"$199.00",andertons:"£199.99",musicstore:"€167.20"}},
-  490: {prices:{zzounds:"$269.99",andertons:"£319.00",gear4music:"£265.00",musicstore:"€251.30"}},
+  490: {prices:{zzounds:"$269.99",andertons:"£319.00",gear4music:"£319.00",musicstore:"€299.00"}},
   491: {prices:{zzounds:"$249.00",amazon:"$249.00",andertons:"£179.00",gear4music:"£170.00",musicstore:"€194.25"}},
   492: {prices:{zzounds:"$449.99",amazon:"$328.98",andertons:"£399.00",gear4music:"£369.00",musicstore:"€335.30"}},
    493: {prices:{zzounds:"$569.00",amazon:"$439.20",andertons:"£459.00",gear4music:"£463.00",musicstore:"€549.00"}},
