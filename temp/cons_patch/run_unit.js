1092695fe1edac64a29d7a663eaee92a9dacd0e2
Danny0885 <danny0885@users.noreply.github.com>
2026-09-20 20:41:39 +0300

    Restore premium-interfaces content in guides.json (productTable EN/ES + faqs) and regenerate premium EN/ES HTML

--- temp/cons_patch/run_unit.js
+++
@@ -1,3 +0,0 @@
-const fs=require('fs');const R='C:/Users/Daniel/projects/topmusiciangear';
-const run=()=>{ try{ require(R+'/temp/cons_patch/gen_shop_buttons_stack.js'); }catch(e){ console.error('ERR '+e.message); } };
-run();
